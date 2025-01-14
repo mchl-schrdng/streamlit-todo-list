3016c0e990d30934ce17d4cb0750f7b0328f4cd1
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2025-01-14 14:22:20 +0100

    update

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ else:
     st.write("No tasks found.")
 
 # Sidebar: Update Existing Task
-st.sidebar.divider()
+st.sidebar.subheader('', divider='rainbow')
 st.sidebar.subheader("Update Existing Task")
 
 if tasks:
@@ -141,7 +141,7 @@ else:
     st.sidebar.write("No tasks available to update.")
 
 # Sidebar: Delete Task
-st.sidebar.divider()
+st.sidebar.subheader('', divider='rainbow')
 st.sidebar.subheader("Delete a Task")
 if tasks:
     task_id_to_delete = st.sidebar.selectbox(
@@ -157,7 +157,7 @@ else:
     st.sidebar.write("No tasks available to delete.")
 
 # Move Reset Database Option to the End of Sidebar
-st.sidebar.divider()
+st.sidebar.subheader('', divider='rainbow')
 if st.sidebar.button("Reset Database (End)"):
     reset_database()
     st.sidebar.success("Database has been reset!")  # Message directly below reset option
